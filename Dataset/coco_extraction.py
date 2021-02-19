64aa28d6c8301b8a58a41b9fa6ac1999182185fb
chekirou hakim <chekirouhakim@gmail.com>
2021-02-20 00:04:00 +0100

    add extraction with original ids

--- Dataset/coco_extraction.py
+++ Dataset/coco_extraction.py
@@ -4,8 +4,8 @@
 import json
 import numpy as np
 
-def extract_coco(inFile, outFile,subset, percentage):
-    """Function to extract a subset from coco
+def extract_coco(inFile, outFile,subset):
+    """Function to extarct a subset from coco
     Parameters:
             infile: annotation file (json)
             outfile: output annotation file (json)
@@ -14,21 +14,31 @@ def extract_coco(inFile, outFile,subset, percentage):
     """
     with open(inFile) as f: #loading json annotation file
         data = json.load(f)
-    Nb_max = int(len(data["images"]) *percentage) # number of images to keep
+    """Nb_max = int(len(data["images"]) *percentage) # number of images to keep
     image_ids = []
     images = data["images"][: Nb_max] # list of kept images
     for i in images: # list of kept image ids
-        image_ids.append(i["id"])
-        
+        image_ids.append(i["id"])"""
+    images_ids = set()    
     annots = [] 
     for annot in data["annotations"]: # filter the annotations
-        if annot["category_id"] in subset and annot["image_id"] in image_ids: # keep the ones about kept images
+        if annot["category_id"] in subset: # keep the ones about kept images
+            annot["category_id"] = subset.index(annot["category_id"]) +1 
             annots.append(annot) # and of categorie in subset
+            images_ids.add(annot["image_id"])
+            
+            
+    images = [] # list of kept images
+    for i in data["images"]: # list of kept image ids
+        if i["id"] in images_ids:
+            images.append(i)
             
     categories = [] # filter the categories
     for elem in data["categories"]:
         if elem["id"] in subset:
+            elem["id"] = subset.index(elem["id"]) +1 
             categories.append(elem)
+    
     
     data["annotations"] = annots # update the data
     data["images"] = images
@@ -37,34 +47,59 @@ def extract_coco(inFile, outFile,subset, percentage):
     with open(outFile, 'w') as data_file: # dump it in the outFile
         data = json.dump(data, data_file)
     return
+    
+def extract_coco_with_same_ids(inFile, outFile,subset):
+    """Function to extarct a subset from coco
+    Parameters:
+            infile: annotation file (json)
+            outfile: output annotation file (json)
+            subset: list of selected class ids
+            percentage: percentage of total images to consider
+    """
+    with open(inFile) as f: #loading json annotation file
+        data = json.load(f)
+    """Nb_max = int(len(data["images"]) *percentage) # number of images to keep
+    image_ids = []
+    images = data["images"][: Nb_max] # list of kept images
+    for i in images: # list of kept image ids
+        image_ids.append(i["id"])"""
+    images_ids = set()    
+    annots = [] 
+    for annot in data["annotations"]: # filter the annotations
+        if annot["category_id"] in subset: # keep the ones about kept images 
+            annots.append(annot) # and of categorie in subset
+            images_ids.add(annot["image_id"])
+            
+            
+    images = [] # list of kept images
+    for i in data["images"]: # list of kept image ids
+        if i["id"] in images_ids:
+            images.append(i)
+            
+    categories = [] # filter the categories
+    for elem in data["categories"]:
+        if elem["id"] in subset:
+            categories.append(elem)
+    
+    
+    data["annotations"] = annots # update the data
+    data["images"] = images
+    data["categories"] = categories
+    
+    with open(outFile, 'w') as data_file: # dump it in the outFile
+        data = json.dump(data, data_file)
+    return
+    
 # ===============================
 # COCO light
 # ===============================
 # train
 inFile = f"/tempory/coco/annotations/instances_train2017.json"
-outFile = f"./coco_light/coco_light_train.json"
-percentage = 0.4
-subset = np.random.choice(90, 20).tolist()
-
+outFile = f"./coco_light/coco_light_train_ids.json"
+subset = [23, 86, 6, 70, 32]
 # test
-extract_coco(inFile, outFile, subset, percentage)
+extract_coco_with_same_ids(inFile, outFile, subset)
 
 inFile = f"/tempory/coco/annotations/instances_val2017.json"
-outFile = f"./coco_light/coco_light_validation.json"
-extract_coco(inFile, outFile, subset, percentage)
-# ===============================
-# COCO test (for debuggin purposes)
-# ===============================
-# train
-
-inFile = f"/tempory/coco/annotations/instances_train2017.json"
-outFile = f"./coco_test/coco_test_train.json"
-percentage = 0.1
-subset = np.random.choice(90, 4).tolist()
-extract_coco(inFile, outFile, subset, percentage)
-
-# test 
-
-inFile = f"/tempory/coco/annotations/instances_val2017.json"
-outFile = f"./coco_test/coco_test_val.json"
-extract_coco(inFile, outFile, subset, percentage)
+outFile = f"./coco_light/coco_light_validation_ids.json"
+extract_coco_with_same_ids(inFile, outFile, subset)
