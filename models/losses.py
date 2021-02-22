1501e7612e0d53942c88a7b0a68c7ada6b24c96a
hanouticelina <hanouticelina@outlook.com>
2021-02-22 20:23:34 +0100

    remove useless modules

--- models/losses.py
+++ models/losses.py
@@ -58,49 +58,6 @@ def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: f
 
     return loss.mean(1).sum() / num_boxes
 
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=None, size_average=True):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha, (float, int)):
-            self.alpha = torch.Tensor([alpha, 1-alpha])
-        if isinstance(alpha, list):
-            self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
-
-    def forward(self, input, target):
-        """
-        :param input: B, L, C
-        :param target: B, L
-        :return:
-        """
-        input = input.view(-1, input.size(-1))
-        target = target.view(-1, 1)
-
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = torch.exp(logpt)
-
-        if self.alpha is not None:
-            at = self.alpha[target].view(-1).type_as(logpt)
-            logpt = logpt * at
-            loss = -1 * (1-pt)**self.gamma * logpt
-
-            if self.size_average:
-                loss = loss.sum() / at.sum()
-                return loss
-            else:
-                return loss.sum()
-        else:
-            loss = -1 * (1-pt)**self.gamma * logpt
-
-            if self.size_average:
-                return loss.mean()
-            else:
-                return loss.sum()
-
 class SetCriterion(nn.Module):
     """ This class computes the loss for DETR.
     The process happens in two steps:
